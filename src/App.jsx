79cd82b9db51fa8a8b574ba6381f0d8ee17da834
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-01-06 19:10:07 -0500

    Added fetch to a weather api

--- src/App.jsx
+++ src/App.jsx
@@ -1,243 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Svgs from './components/Svgs';
+import Day from './components/Day';
 import './App.scss';
 
 function App() {
-  const daysOfWeekMap = {
-    0: 'SUN',
-    1: 'MON',
-    2: 'TUES',
-    3: 'WED',
-    4: 'THUR',
-    5: 'FRI',
-    6: 'SAT',
-  };
-  const setValue = (e, value) => {
-    e.innerHTML = value;
-  };
+  const [lat, setLat] = useState();
+  const [long, setLong] = useState();
+  const [data, setData] = useState();
   useEffect(() => {
-    const keys = document.querySelectorAll('div .day-of-week');
-    keys.forEach((key, index) => {
-      setValue(key, daysOfWeekMap[(new Date().getDay() + index) % 7]);
-    });
+    const fetchData = async () => {
+      await navigator.geolocation.getCurrentPosition((position) => {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+    };
+    fetchData();
   }, []);
+
   useEffect(() => {
-    const keys = document.querySelectorAll('div .date');
-    keys.forEach((key, index) => {
-      setValue(key, (new Date(new Date().setDate(new Date().getDate() + index))).getDate());
-    });
-  }, []);
+    const fetchData = async () => {
+      if (long && lat) {
+        const res = await fetch(`${process.env.REACT_APP_API_URL}/forecast?lat=${lat}&lon=${long}&cnt=7&appid=${process.env.REACT_APP_API_KEY}`);
+        const resdata = await res.json();
+        setData(resdata);
+      }
+    };
+    fetchData();
+  }, [long]);
   return (
     <>
       <div className="wrapper">
-        <div className="day">
-          <div className="day-of-week" />
-          <div className="date" />
-          <div className="bar cloudy">
-            <div className="weather">
-              <svg role="img">
-                <use
-                  xlinkHref="#cloudy"
-                  width={264}
-                  height={166}
-                  viewBox="0 0 264 166"
-                />
-              </svg>
-            </div>
-            <div className="temperature">
-              72
-              <span className="degrees">°</span>
-            </div>
-            <div className="content">
-              <div className="precipitation">
-                <svg role="img" className="icon">
-                  <use xlinkHref="#precipitation" />
-                </svg>
-                84%
-              </div>
-              <div className="low">
-                <svg role="img" className="icon">
-                  <use xlinkHref="#low" />
-                </svg>
-                28°
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="day">
-          <div className="day-of-week" />
-          <div className="date" />
-          <div className="bar sunny">
-            <div className="weather">
-              <svg role="img" width={208} height={213} viewBox="0 0 208 213">
-                <use xlinkHref="#sunny" />
-              </svg>
-            </div>
-            <div className="temperature">
-              65
-              <span className="degrees">°</span>
-            </div>
-            <div className="content">
-              <div className="precipitation">
-                <svg role="img" className="icon">
-                  <use xlinkHref="#precipitation" />
-                </svg>
-                84%
-              </div>
-              <div className="low">
-                <svg role="img" className="icon">
-                  <use xlinkHref="#low" />
-                </svg>
-                28°
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="day">
-          <div className="day-of-week" />
-          <div className="date" />
-          <div className="bar stormy">
-            <div className="weather">
-              <svg role="img" width={246} height={187} viewBox="0 0 246 187">
-                <use xlinkHref="#stormy" />
-              </svg>
-            </div>
-            <div className="temperature">
-              67
-              <span className="degrees">°</span>
-            </div>
-            <div className="content">
-              <div className="precipitation">
-                <svg role="img" className="icon">
-                  <use xlinkHref="#precipitation" />
-                </svg>
-                84%
-              </div>
-              <div className="low">
-                <svg role="img" className="icon">
-                  <use xlinkHref="#low" />
-                </svg>
-                28°
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="day">
-          <div className="day-of-week" />
-          <div className="date" />
-          <div className="bar snowy">
-            <div className="weather">
-              <svg role="img" width={230} height={196} viewBox="0 0 230 196">
-                <use xlinkHref="#snowy" />
-              </svg>
-            </div>
-            <div className="temperature">
-              32
-              <span className="degrees">°</span>
-            </div>
-            <div className="content">
-              <div className="precipitation">
-                <svg role="img" className="icon">
-                  <use xlinkHref="#precipitation" />
-                </svg>
-                84%
-              </div>
-              <div className="low">
-                <svg role="img" className="icon">
-                  <use xlinkHref="#low" />
-                </svg>
-                28°
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="day">
-          <div className="day-of-week" />
-          <div className="date" />
-          <div className="bar partly-cloudy">
-            <div className="weather">
-              <svg role="img" width={230} height={209} viewBox="0 0 230 209">
-                <use xlinkHref="#partly-cloudy" />
-              </svg>
-            </div>
-            <div className="temperature">
-              57
-              <span className="degrees">°</span>
-            </div>
-            <div className="content">
-              <div className="precipitation">
-                <svg role="img" className="icon">
-                  <use xlinkHref="#precipitation" />
-                </svg>
-                84%
-              </div>
-              <div className="low">
-                <svg role="img" className="icon">
-                  <use xlinkHref="#low" />
-                </svg>
-                28°
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="day">
-          <div className="day-of-week" />
-          <div className="date" />
-          <div className="bar rainy">
-            <div className="weather">
-              <svg role="img" width={160} height={222} viewBox="0 0 160 222">
-                <use xlinkHref="#rainy" />
-              </svg>
-            </div>
-            <div className="temperature">
-              63
-              <span className="degrees">°</span>
-            </div>
-            <div className="content">
-              <div className="precipitation">
-                <svg role="img" className="icon">
-                  <use xlinkHref="#precipitation" />
-                </svg>
-                84%
-              </div>
-              <div className="low">
-                <svg role="img" className="icon">
-                  <use xlinkHref="#low" />
-                </svg>
-                28°
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="day">
-          <div className="day-of-week" />
-          <div className="date" />
-          <div className="bar sunny">
-            <div className="weather">
-              <svg role="img" width={208} height={213} viewBox="0 0 208 213">
-                <use xlinkHref="#sunny" />
-              </svg>
-            </div>
-            <div className="temperature">
-              71
-              <span className="degrees">°</span>
-            </div>
-            <div className="content">
-              <div className="precipitation">
-                <svg role="img" className="icon">
-                  <use xlinkHref="#precipitation" />
-                </svg>
-                84%
-              </div>
-              <div className="low">
-                <svg role="img" className="icon">
-                  <use xlinkHref="#low" />
-                </svg>
-                28°
-              </div>
-            </div>
-          </div>
-        </div>
+        {data ? data.list.map((e, index) => <Day props={e} key={e.dt} index={index} />) : null}
       </div>
       <Svgs />
     </>
